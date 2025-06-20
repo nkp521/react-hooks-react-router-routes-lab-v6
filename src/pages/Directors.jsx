d54bdfda8367120445ae31d162aaa2e96e6a563e
Nihar Patel <niharpatel52@gmail.com>
2025-06-20 11:36:32 -0400

    Display Directors on Directors Page

--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -2,12 +2,37 @@ import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+      .then((res) => res.json())
+      .then((data) => setDirectors(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const renderDirectors = () => {
+    return directors.map((director) => (
+      <article key={director.id}>
+        <h2>{director.name}</h2>
+        <ul>
+          {director.movies.map((movie, id) => (
+            <li key={id}>{movie}</li>
+          ))}
+        </ul>
+      </article>
+    ));
+  };
+
   return (
     <>
       <header>
         <NavBar />
       </header>
-      <main>{/* Director info here! */}</main>
+      <main>
+        <h1>Directors Page</h1>
+        {renderDirectors()}
+      </main>
     </>
   );
 }
