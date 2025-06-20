2b9578e1d8f113763a98691f62d0992d20ba9f74
Nihar Patel <niharpatel52@gmail.com>
2025-06-20 11:56:49 -0400

    Add Error Handeling

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+export default function ErrorPage() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Oops! Looks like something went wrong.</h1>
+      </main>
+    </>
+  );
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,23 +2,28 @@ import Home from "./pages/Home";
 import Directors from "./pages/Directors";
 import Actors from "./pages/Actors";
 import Movie from "./pages/Movie";
+import ErrorPage from "./components/ErrorPage";
 
 const routes = [
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/Directors",
     element: <Directors />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/Actors",
     element: <Actors />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/Movie",
     element: <Movie />,
+    errorElement: <ErrorPage />,
   },
 ];
 
